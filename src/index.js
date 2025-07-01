6ec839de187b53b1d8bc7428293f77d5333e5e65
thestarsahil <sahilali88827@gmail.com>
2025-07-01 23:23:39 +0530

    app.js file code modify

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ dotenv.config({
     path: "./env"
 });
 
-connnectDB();
-
-
-
-
-
-
-
-
+connnectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port: ${process.env.PORT }`);
+    });
+    console.log("Database connected");
+})
+.catch((err) => {
+    console.log(err);
+})
 
 
 
